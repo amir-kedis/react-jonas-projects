cffff5651e196ee7e799db8d7218adcd7aa958fc
amir-kedis <amir.kedis@gmail.com>
2024-07-28 14:12:25 +0300

    feat(react-quiz): create fetching questions from fake server functionality

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,7 +1,61 @@
-import "./App.css";
+import { useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import { useReducer } from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+
+const initState = {
+  questions: [],
+  // "loading", "ready", "active", "error", "finished"
+  status: "loading",
+  error: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "DataSuccess":
+      return {
+        ...state,
+        status: "ready",
+        questions: action.payload,
+      };
+    case "DataFailed":
+      return {
+        ...state,
+        status: "error",
+        error: action.payload,
+      };
+    default:
+      throw new Error("Unkown Action");
+  }
+}
 
 function App() {
-  return <></>;
+  const [{ questions, status }, dispatch] = useReducer(reducer, initState);
+
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "DataSuccess", payload: data }))
+      .catch((err) =>
+        dispatch({
+          type: "DataFailed",
+          payload: "Failed to Load Questions" + err.message,
+        }),
+      );
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <Error />}
+      </Main>
+    </div>
+  );
 }
 
 export default App;
